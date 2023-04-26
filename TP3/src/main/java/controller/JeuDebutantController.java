749efdc10a7b13af3c8e104d4aa128eae25dadd1
AC-climoilou <1701633@cegeplimoilou.ca>
2023-04-26 16:52:20 -0400

    Correction de l'erreur du commit

--- TP3/src/main/java/controller/JeuDebutantController.java
+++ TP3/src/main/java/controller/JeuDebutantController.java
@@ -11,7 +11,6 @@ public class JeuDebutantController extends Application {
 
     private Grille grille;
 
-
     @Override
     public void start(Stage primaryStage) throws Exception {
 
@@ -21,27 +20,18 @@ public class JeuDebutantController extends Application {
 
         Scene scene = new Scene(fxmlLoader.load(), 263, 288);
         primaryStage.setScene(scene);
-<<<<<<< Updated upstream
+
         GrilleController controllerTest = new GrilleController();
         grille = new Grille(4,4);
         controllerTest.PeuplerGrille(grille, 10);
         int i = 0;
-=======
-        primaryStage.setResizable(false);
->>>>>>> Stashed changes
-        primaryStage.show();
 
+        primaryStage.setResizable(false);
+        primaryStage.show();
 
     }
 
-<<<<<<< Updated upstream
-
-
-
-    public static void main(String args[]) {
-=======
     public static void main(String[] args) {
->>>>>>> Stashed changes
         launch(args);
     }
 }
