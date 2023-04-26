8df7246aeb6a6d317744c91a23b352d83b019dc0
elie987654321 <elie987654321@gmail.com>
2023-04-26 14:17:34 -0400

    Premier commit
    Premier commit.
    Le projet compile correctement mais nous avont uniquement effectuer des tests superficiel pour l'instant

---
+++ TP3/src/main/java/Demineur/Demineur/src/demin/Main.java
@@ -0,0 +1,19 @@
+package Demineur.Demineur.src.demin;
+
+import java.util.*;
+import javax.swing.*;
+
+
+public class Main {
+
+  public static void main(String[] args) {
+    try {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+      
+    }
+    catch(Exception e) {
+      e.printStackTrace();
+    }
+    new Demineur(16,30,99,3);//hop, on lance le jeux en expert
+  }
+}
